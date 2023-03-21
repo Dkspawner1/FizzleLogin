46817a8b57b35ccf83f6abc938cc0ca4cf86db74
Dkspawner1 <Nicholaschrysanthou123@gmail.com>
2023-03-21 04:26:20 +0000

    init commit

---
+++ Person.cs
@@ -0,0 +1,7 @@
+#pragma once
+
+typedef struct Person
+{
+    char* username;
+    char* password;
+};
---
+++ source.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Person.h"
+int main(int argc, char *argv[])
+{
+    Person p;
+    return EXIT_SUCCESS;
+}
